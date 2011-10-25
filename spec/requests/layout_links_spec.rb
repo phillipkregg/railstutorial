12975e5b032bffd57de58fc3893a3a8fcaf33928
PhillipKregg <phillipkregg@gmail.com>
2011-10-25 16:22:44 -0500

    Added user model validations

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -37,7 +37,7 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Home")
     click_link "Sign up now"
     response.should have_selector('title', :content => "Sign up")
-    #response.should have_selector('a[href="/"]>img')
+    response.should have_selector('a[href="/"]>img')
   end  
   
   
