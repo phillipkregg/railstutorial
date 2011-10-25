1d0ec37a5206ffac2c3e486e5b6e938db93968c1
PhillipKregg <phillipkregg@gmail.com>
2011-10-25 16:54:49 -0500

    Added a stub User show page

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+# faster speeds all around
+require File.dirname(__FILE__) + "/../lib/faster_require-0.9.2/lib/faster_require" 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
